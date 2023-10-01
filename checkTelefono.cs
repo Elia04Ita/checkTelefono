f35d0f1e161372165803fff3231fb7a0e2bfe1e6
Veschi Elia <89824404+Elia04Ita@users.noreply.github.com>
2023-10-01 22:46:25 +0200

    Add files via upload

---
+++ checkTelefono.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace checkTelefono
+{
+    public static class Telefono
+    {
+        public static string Check(string[] input)
+        {
+            foreach (string s in input)
+            {
+                // Converte l'array in string
+                string ntelefono = Uniscintelefono(s);
+
+                // Verifica se la stringa è vuota o troppo corta
+                if (ntelefono.Length < 10 || ntelefono.Length > 14)
+                {
+                    continue;
+                }
+
+                // Controlla se la stringa inizia con +39, 0039 o 3 e se è della giusta lunghezza
+                if (ntelefono.StartsWith("+39") && ntelefono.Length == 13)
+                {
+                    return s;
+                }
+                if (ntelefono.StartsWith("0039") && ntelefono.Length == 14)
+                {
+                    return s;
+                }
+                if (ntelefono.StartsWith("3") && ntelefono.Length == 10)
+                {
+                    return s;
+                }
+            }
+
+            // Se nessun numero segue i parametri restituisce ""
+            return "";
+
+            //Unisce i caratteri in uno string
+            static string Uniscintelefono(string input)
+            {
+                string stringunita = "";
+                foreach (char c in input)
+                {
+                    //Controlla che inizii con un numero o il simbolo "+"
+                    if (c >= '0' && c <= '9' || c == '+')
+                    {
+                        stringunita += c;
+                    }
+                }
+                return stringunita;
+            }
+        }
+    }
+}
